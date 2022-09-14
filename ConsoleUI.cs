4f1563bf83b7fd1e3f23a4a6132f832ebbe413f1
Chowon Jung <dancing4am@gmail.com>
2022-09-14 11:43:38 -0400

    Basic console UI structure created

--- ConsoleUI.cs
+++ ConsoleUI.cs
@@ -16,23 +16,84 @@ namespace SQL_Devnote
 
         internal void MainMenu()
         {
-            Logo();
-            NumMenu(1, "Manage Database");
-            NumMenu(2, "Search Data");
+            do
+            {
+                Console.Clear();
+                Logo();
+                NumMenu(1, "Manage Database");
+                NumMenu(2, "Search Data");
+                NumMenu(3, "Exit");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("    Enter the number: ");
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Enter the number: ");
-            GetUserInput();
+                switch (ValidateToInt(GetUserInput()))
+                {
+                    case 1:
+                        DataMenu();
+                        break;
+                    case 2:
+                        SearchMenu();
+                        break;
+                    case 3:
+                        ExitApplication();
+                        break;
+                    default:
+                        PrintError("Invalid input");
+                        break;
+                }
+            } while (true);
         }
 
         internal void DataMenu()
         {
+            do
+            {
+                Console.Clear();
+                Logo();
+                NumMenu(1, "Add");
+                NumMenu(2, "Update");
+                NumMenu(3, "Remove");
+                NumMenu(4, "Main Menu");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("    Enter the number: ");
 
+                switch (ValidateToInt(GetUserInput()))
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        PrintError("Invalid input");
+                        break;
+                }
+            } while (true);
         }
 
         internal void SearchMenu()
         {
 
+        }
+
+        private int ValidateToInt(string? input)
+        {
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         private string? GetUserInput()
@@ -46,35 +107,49 @@ namespace SQL_Devnote
             }
             catch (Exception e)
             {
-                Console.WriteLine();
-                Console.ForegroundColor = warnColor;
-                Console.Beep();
-                Console.Error.WriteLine(e);
-                Console.ForegroundColor = baseColor;
-                Console.Write("Press Any Key to Continue . . .");
-                Console.ReadKey();
+                PrintError(e.Message);
                 Console.Clear();
             }
 
             return input;
         }
 
+
+        private void ExitApplication()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = baseColor;
+            Console.WriteLine(" See ya!");
+            Environment.Exit(0);
+        }
+
+        private void PrintError(string msg)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = warnColor;
+            Console.Beep();
+            Console.Error.WriteLine(msg);
+            Console.ForegroundColor = baseColor;
+            Console.Write(" Press Any Key to Continue . . .");
+            Console.ReadKey();
+        }
+
         private void Logo()
         {
             Console.WriteLine();
             Console.ForegroundColor = logoColor;
-            Console.WriteLine(" _____       ");
-            Console.WriteLine(" |  _ \\  _____   ___ __   ___ | |_ ___ ");
-            Console.WriteLine(" | | | |/ _ \\ \\ / / '_ \\ / _ \\| __/ _ \\");
-            Console.WriteLine(" | |_| |  __/\\ V /| | | | (_) | || __ /");
-            Console.WriteLine(" |____/ \\___| \\_/ |_| |_|\\___/ \\__\\___|");
+            Console.WriteLine("     _____       ");
+            Console.WriteLine("     |  _ \\  _____   ___ __   ___ | |_ ___ ");
+            Console.WriteLine("     | | | |/ _ \\ \\ / / '_ \\ / _ \\| __/ _ \\");
+            Console.WriteLine("     | |_| |  __/\\ V /| | | | (_) | || __ /");
+            Console.WriteLine("     |____/ \\___| \\_/ |_| |_|\\___/ \\__\\___|");
             Console.WriteLine();
         }
 
         private void NumMenu(int num, string text)
         {
             Console.ForegroundColor = numColor;
-            Console.Write($"{num}) ");
+            Console.Write($"    {num}) ");
             Console.ForegroundColor = menuColor;
             Console.WriteLine($"{text}");
         }
