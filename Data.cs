93f7de1e151c4c98b8219b21b705dfff00ae8d3f
Chowon Jung <dancing4am@gmail.com>
2022-09-29 11:34:40 -0400

    git ignore added

--- Data.cs
+++ Data.cs
@@ -19,7 +19,7 @@ namespace SQL_Devnote
         public string description;
         public Tags tags;               // array? list?
         public string constructor;              // not all classes implements the same structure... we need to dynamically create a class type. but until before then... put this here. (Reflection feature)
-        public List<string> fields;             // would a dictionary be better here? or handle it using query when getting/setting it?
+        public Fields fields;                   // would a dictionary be better here? or handle it using query when getting/setting it?
         public List<string> properties;         // or polymorphism? abstract class?
         public List<string> methods;            // have basic structure of class, and allow additional sections?
         public List<string> operators;          // use of generic class?
@@ -36,7 +36,7 @@ namespace SQL_Devnote
         {
             FieldInfo[] fieldInfo = this.GetType().GetFields();
             return (from field in fieldInfo
-                   select field.Name).ToArray();                // maybe can be used to call different properties from custom data using expression-dynamic functions? should it be returned in FiledInfo type to access?
+                   select field.Name).ToArray() as string[];                // maybe can be used to call different properties from custom data using expression-dynamic functions? should it be returned in FiledInfo type to access?
         }
 
         /*
@@ -88,10 +88,7 @@ namespace SQL_Devnote
 
         public void Add(string tag) => list.Add(tag);           // expression bodied method
         public void Remove(string tag) => list.Remove(tag);
-
-        //public int Capacity() => list.Capacity;                 // this is read-only
         public string this[int index] => list[index];           // read-only indexer
-
     }
 
     internal class Fields
@@ -100,12 +97,18 @@ namespace SQL_Devnote
         public void Add(Field field) => list.Add(field);
         public void Remove(string name)
         {
-            Field? field = list.Find(f => f.Name == name);
+            Field? field = GetField(name);
             if (field is not null)
             {
                 list.Remove(field);
             }
         }
+        public string[] GetFieldList() => (from field in list
+                                           orderby field.Name
+                                           select field.Name).ToArray() as string[];
+
+        public Field? GetField(string name) => list.Find(f => f.Name == name);
+        public Field[] GetAllFields() => list.ToArray();
     }
 
     internal class Field
@@ -119,6 +122,32 @@ namespace SQL_Devnote
         public Field(string name)
         {
             Name = name;
+            Value = string.Empty;
+            Definition = string.Empty;
+            Remarks = string.Empty;
+            Example = string.Empty;
+        }
+    }
+
+    internal class Methods
+    {
+
+    }
+
+    internal class Method
+    {
+        public string Name { get; set; }
+        public string Definition { get; set; }
+//        public List<string, string> Parameters 
+        public string Return { get; set; }
+        public string Remarks { get; set; }
+        public string Example { get; set; }
+        public Method(string name)
+        {
+            Name = name;
+            Definition = string.Empty;
+            Remarks = string.Empty;
+            Example = string.Empty;
         }
     }
 }
